8110c228b4109e5530e7e352aaa8e4bcdd1bb7ea
abeertech01 <abeer.technology@gmail.com>
2024-12-13 20:45:37 +0600

    mode toggle added

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react"
+import Image from "next/image"
+import logoImage from "@/assets/knight.png"
+import ModeToggle from "@/components/ModeToggle"
+
+interface ComponentProps {}
+
+const page: FC<ComponentProps> = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-5">
+      <div className="absolute top-4 left-4">
+        <Image width={40} height={40} alt="logo" src={logoImage} />
+      </div>
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
+      {/* <AuthTabs /> */}
+    </div>
+  )
+}
+
+export default page
