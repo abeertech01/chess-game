7b1abfeda82bc45cafd8d865ba9e023b32373fcb
abeertech01 <abeer.technology@gmail.com>
2025-01-01 00:41:52 +0600

    marking squares color changed

--- src/app/(private)/playground/components/Square.tsx
+++ src/app/(private)/playground/components/Square.tsx
@@ -27,15 +27,21 @@ const Square: FC<ComponentProps> = ({
       toggleSelectedSquare()
     }
   }
-
+  // after:absolute after:w-[calc(100%-1rem)] after:h-[calc(100%-1rem)] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 border-2 border-rose-500 rounded-md
   return (
     <div
       onClick={clickSquare}
-      className={`square w-[5rem] h-[5rem] text-black flex justify-center items-center ${
-        (i + j) % 2 === 0 ? "bg-[#ffffff]" : "bg-emerald-700"
+      className={`relative square w-[5rem] h-[5rem] text-black flex justify-center items-center ${
+        (i + j) % 2 === 0 ? "bg-[#ffffff]" : "bg-teal-700"
       } ${squareObj.piece ? "cursor-pointer" : ""} ${
-        squareObj.isSelected ? "border-4 border-rose-500" : ""
-      } ${squareObj.isToMove ? "border-4 border-amber-300" : ""}`}
+        squareObj.isSelected
+          ? "after:absolute after:w-[calc(100%-0.6rem)] after:h-[calc(100%-0.6rem)] after:left-1/2 after:-translate-x-1/2 after:top-1/2 after:-translate-y-1/2 after:border-4 after:border-rose-500 after:rounded-md"
+          : ""
+      } ${
+        squareObj.isToMove
+          ? "after:absolute after:w-[calc(100%-0.6rem)] after:h-[calc(100%-0.6rem)] after:left-1/2 after:-translate-x-1/2 after:top-1/2 after:-translate-y-1/2 after:border-4 after:border-cyan-400 after:rounded-md"
+          : ""
+      }`}
       key={j}
     >
       <h1>{squareObj.piece ? null : squareObj.id}</h1>
